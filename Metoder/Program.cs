2f64c1eb01ab065c7e571e838f2b339e28f7ecf9
Mikael <mikael.bergstrom@ga.ntig.se>
2020-09-03 15:05:14 +0200

    Initial commit

---
+++ Metoder/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Metoder
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Hello World!");
+
+      LivingRoomDescription();
+
+      Console.ReadLine();
+
+    }
+
+    static void LivingRoomDescription()
+    {
+      Console.WriteLine("It's large and smelly.");
+
+      // LivingRoomDescription();
+
+    }
+  }
+}
